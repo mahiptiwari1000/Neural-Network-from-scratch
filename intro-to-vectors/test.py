5b2e5e119314d2c18af056c74ef669d7975aa2e4
Mahip Tiwari <mahiptiwari@Mahips-MacBook-Air.local>
2025-11-19 00:02:06 -0600

    add vector functions like addition and multiplication in the class

--- intro-to-vectors/test.py
+++ intro-to-vectors/test.py
@@ -1,9 +1,30 @@
 from vector import Vector
 
-v1 = Vector(1,2,3)
-v2 = Vector(4,5,6)
+v1 = Vector([1,2,3])
+v2 = Vector([4,5,6])
 
 print(v1, '' , v2)
 
 print(len(v1))
 print(v1 == v2)
+
+x = Vector([1,2,3])
+print(x)
+print(x.coords)
+
+print(x[0])
+
+x[0] = 42
+print(x[0])
+print(x)
+
+print([1,2].__add__([3,4])) #by default __add__ method in python concatenates and does not add
+print([1,2] + [3,4])# same behavior here
+
+##To resolve this problem to add vectors, we should define a custom method in the Vector class
+
+print(v1 + v2)
+
+x = Vector([1,2,3])
+print(x * 2.0)
+print(2.0 * x)
--- intro-to-vectors/vector.py
+++ intro-to-vectors/vector.py
@@ -1,6 +1,6 @@
 class Vector:
-    def __init__(self, *coords):
-        self.coords = tuple(coords)
+    def __init__(self, coords):
+        self.coords = list(coords)
     
     def __repr__(self):
         return f"Vector{self.coords}"
@@ -10,3 +10,19 @@ class Vector:
     
     def __eq__(self, other):
         return self.coords == other.coords
+    
+    def __getitem__(self,idx):
+        return self.coords[idx]
+    
+    def __setitem__(self, idx, value):
+        self.coords[idx] = value
+
+    def __add__(self, other):
+        return Vector([x + y for x,y in zip(self.coords, other.coords)])
+    
+    def __mul__(self, other):
+        return Vector([other * x for x in self.coords])
+    
+    def __rmul__(self, other):
+        return self.__mul__(other)
+    
